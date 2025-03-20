6eec149aa08870593ad8fe43fd05134829e550f6
yinjiangqun <yinjiangqun@126.com>
2025-03-20 21:27:27 +0800

    工作流配置修复第9次

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,36 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { useUserStore } from "../stores/user";
+import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '../stores/user'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: () => import("@/views/Home.vue"),
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
   },
   {
-    path: "/login",
-    name: "Login",
-    component: () => import("@/views/Login.vue"),
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue'),
   },
   {
-    path: "/register",
-    name: "Register",
-    component: () => import("@/views/Register.vue"),
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/Register.vue'),
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
 
 router.beforeEach((to, from, next) => {
-  const userStore = useUserStore();
+  const userStore = useUserStore()
   if (to.meta.requiresAuth && !userStore.isAuthenticated) {
-    next("/login");
+    next('/login')
   } else {
-    next();
+    next()
   }
-});
+})
